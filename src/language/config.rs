043cd8dddf932d7025e806239503aef42e855fcb
treymouledoux <72358826+treymouledoux@users.noreply.github.com>
2026-09-01 17:24:51 -0500

    started work on default config and ver comp

--- src/language/config.rs
+++ src/language/config.rs
@@ -1,1 +1,31 @@
-//toml manager
+struct Config {
+    metaver: u8,
+    mingrid9ver: String,
+    author: String,
+    description: String,
+    version: u8,
+    showmetadata: bool,
+    advancedparse: bool,
+    dontcache: bool,
+    echogridmod: bool,
+    nolog: bool,
+    verbosity: u8,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            metaver: 2,
+            mingrid9ver: "2026.1.0".to_owned(),
+            author: "unknwon".to_owned(),
+            description: "unknown".to_owned(),
+            version: 0,
+            showmetadata: false,
+            advancedparse: false,
+            dontcache: false,
+            echogridmod: false,
+            nolog: false,
+            verbosity: 0,
+        }
+    }
+}
