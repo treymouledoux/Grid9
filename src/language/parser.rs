da8c387f721312356f6aa6a52490464ef1b94cd7
treymouledoux <72358826+treymouledoux@users.noreply.github.com>
2026-09-06 23:51:21 -0500

    improved hashing and parser cache system

--- src/language/parser.rs
+++ src/language/parser.rs
@@ -13,21 +13,22 @@ use sha2::{Digest, Sha256};
 
 use crate::{file_man::PARSER_CACHE_DIR, language::config::Config};
 
-pub fn parse(file_path: &PathBuf, cfg: Config) -> String {
+pub fn parse(file_path: &PathBuf, mut cfg: Config) -> String {
     if cfg.verbosity >= 2 {
         logf!(Info, "Parsing script");
     }
 
     let file = read_to_string(file_path).log_expect(Error, "Failed to read script file");
 
-    let digest = Sha256::digest(file.as_bytes());
-    let mut file_hash = String::with_capacity(64);
-    for byte in digest {
-        let _ = write!(file_hash, "{:02x}", byte);
+    let mut file_hash: String = Sha256::digest(file.as_bytes())
+        .iter()
+        .map(|b| format!("{b:02x}"))
+        .collect();
+    if cfg.advanced_parse {
+        file_hash.push_str("_ap");
     }
 
-    // Checks to allow parser cache load
-    // Is allowed if dont_cache is false and if a file exists with the same hash
+    // Checks to allow parser cache load if dont_cache is false and if a file exists with the same hash
     if !cfg.dont_cache
         && Path::new(&format!(
             "{}/{}.g9",
@@ -43,8 +44,23 @@ pub fn parse(file_path: &PathBuf, cfg: Config) -> String {
             logf!(Info, "Loading cached code from parser cache");
         }
 
-        return read_to_string(file_path).log_expect(Error, "Failed to read parser cache file");
+        cfg.dont_cache = true;
+
+        return read_to_string(Path::new(&format!(
+            "{}/{}.g9",
+            PARSER_CACHE_DIR
+                .clone()
+                .into_string()
+                .log_expect(Error, "Failed to load parser cache"),
+            file_hash
+        )))
+        .unwrap_or({
+            logf!(Warning, "Failed to read found cached parsed code, forcing reparse, it is recommended to clean your cache dirrectory with the following command: grid9 c parser_cache");
+            parse(file_path, cfg)
+        });
     }
 
-    "gurt".to_string()
+    let mut parsed_code = file;
+
+    parsed_code
 }
